9805114eb7bd4b10f80c027567d33c5de80d9ce3
Sean Tolentino <sean@works-progress.com>
2016-10-29 19:17:08 -0400

    Added add and subtract functionality to buttons

--- app.jsx
+++ app.jsx
@@ -28,19 +28,34 @@ Header.propTypes = {
   title: React.PropTypes.string.isRequired,
 };
 
-function Counter(props) {
-  return (
-    <div className="counter">
-      <button className="counter-action decrement"> - </button>
-      <div className="counter-score">{props.score}</div>
-      <button className="counter-action increment"> + </button>
-    </div>
-  );
-}
-
-Counter.propTypes = {
-  score: React.PropTypes.number.isRequired
-};
+var Counter = React.createClass({
+  propTypes: {
+  },
+  getInitialState: function() {
+    return {
+      score: 0,
+    }
+  },
+  incrementScore: function() {
+    this.setState({
+      score: (this.state.score + 1)
+    })
+  },
+  decrementScore: function() {
+    this.setState({
+      score: (this.state.score - 1)
+    })
+  },
+  render: function() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+        <div className="counter-score">{this.state.score}</div>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+      </div>
+    );
+  }
+});
 
 function Player(props) {
   return (
@@ -49,7 +64,7 @@ function Player(props) {
         {props.name}
       </div>
       <div className="player-score">
-        <Counter score={props.score}/>
+        <Counter />
       </div>
     </div>
   )
