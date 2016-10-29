7b2098e8621e7bdee2011ae891c69ad4710c712d
Sean Tolentino <sean@works-progress.com>
2016-10-29 17:45:46 -0400

    Adding styles and sample static data

--- app.jsx
+++ app.jsx
@@ -1,6 +1,47 @@
-function Application () {
+function Application() {
   return (
-    <div className="application">
+    <div className="scoreboard">
+      <div className="header">
+        <h1>Scoreboard</h1>
+      </div>
+      <div className="players">
+        <div className="player">
+          <div className="player-name">
+            Sean Tolentino
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">31</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+        <div className="player">
+          <div className="player-name">
+            Ryan Tolentino
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">41</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+        <div className="player">
+          <div className="player-name">
+            Sebastian Tolentino
+          </div>
+          <div className="player-score">
+            <div className="counter">
+              <button className="counter-action decrement"> - </button>
+              <div className="counter-score">51</div>
+              <button className="counter-action increment"> + </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
