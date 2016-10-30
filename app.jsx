b41b2e2d9031a427b7f5824cfde16962b0e6a7a2
Sean Tolentino <sean@works-progress.com>
2016-10-30 02:22:01 -0400

    Add stopwatch with stop and start ability

--- app.jsx
+++ app.jsx
@@ -18,6 +18,65 @@ var PLAYERS = [
 
 var nextId = 4;
 
+var Stopwatch = React.createClass({
+  getInitialState: function() {
+    return {
+      running: false,
+      elapsedTime: 0,
+      previousTime: 0,
+    };
+  },
+
+  componentDidMount: function() {
+    this.interval = setInterval(this.onTick, 100);
+  },
+
+  componentWillUnmount: function() {
+    clearInterval(this.interval);
+  },
+
+  onTick: function() {
+    //console.log("on tick");
+    if (this.state.running) {
+      var now = Date.now();
+      this.setState({
+        previousTime: now,
+        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
+      });
+    }
+  },
+
+  onStart: function () {
+    this.setState({
+      running: true,
+      previousTime: Date.now(),
+    });
+  },
+
+  onStop: function () {
+    this.setState({ running: false, });
+  },
+
+  onReset: function () {
+    this.setState({
+      elapsedTime: 0,
+      previousTime: Date.now(),
+    });
+  },
+
+  render: function () {
+    var seconds = Math.floor(this.state.elapsedTime / 1000);
+    return (
+      <div className="stopwatch">
+        <h2>Stopwatch</h2>
+        <div className="stopwatch-time">{seconds}</div>
+        { this.state.running ? <button onClick={this.onStop}>Stop</button> : <button onClick={this.onStart}>Start</button> }
+        <button onClick={this.onReset}>Reset</button>
+      </div>
+    );
+  },
+});
+
 var AddPlayerForm = React.createClass({
   propTypes: {
     onAdd: React.PropTypes.func.isRequired,
@@ -86,6 +145,7 @@ function Header(props) {
     <div className="header">
       <Stats players={props.players}/>
       <h1>{props.title}</h1>
+      <Stopwatch />
     </div>
   );
 }
